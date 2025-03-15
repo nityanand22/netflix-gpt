3b63ed144676fc0f2a11c8fa50ebf8d26316baf6
Nityanand <you@example.com>
2025-03-15 23:56:50 +0530

    update key

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -4,6 +4,7 @@ import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
 import GptSearch from "./GptSearch";
+import { useSelector } from "react-redux";
 
 const Browse = () => {
   const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
